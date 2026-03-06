f4c445ee883f65e5aea8acf96926f5ea07fee9ec
CZalbert6 <23300041@uttt.edu.mx>
2026-03-05 18:57:07 -0600

    Activando despliegue automatico de GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   // URL de tu sitio en GitHub Pages
   site: 'https://CZalbert6.github.io',
@@ -9,11 +8,10 @@ export default defineConfig({
   // Nombre exacto de tu repositorio para que las rutas funcionen
   base: '/extra-earth',
   
-  // Fuerza a Astro a generar archivos estáticos para GitHub Pages
+  // Fuerza a Astro a generar archivos estáticos
   output: 'static',
   
   server: {
-    // Esto ayuda a que el servidor local sea más estable
     port: 4321,
     host: true
   }
